d251041e6db40ef848903cd7f77bacf802e82f77
Apprentice <apprentice@dbc23.local>
2013-09-26 15:05:39 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenlight::Application.config.secret_token = '2df782bd00c124129c8b3e15f19e65dd0a0252a422aaadb99c07a139884681b3a748a4ff7ea7ab912341215f70b85b8097600128875034c1eacf7e76f735756f'
